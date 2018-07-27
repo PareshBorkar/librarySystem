1d3c06cabfde1a3a8617429cbe48202861177f21
Pareshb <pareshb@opspl.com>
2018-07-27 19:07:07 +0530

    port number fixes

--- app/server/index.js
+++ app/server/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 // import express from 'express'
 var app = express();
 
-app.listen(1023, function (err) {
+app.listen(1024, function (err) {
     if (!err) {
-        console.log('Express Server has started on port: 1023');
+        console.log('Express Server has started on port: 1024');
     } else {
         console.log('Error starting the server. Either the port is already in use or something is wrong with the express configuration.');
     }
